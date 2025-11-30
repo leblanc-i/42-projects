f7ce41c3990bfa454c3cfdbd059c9afc1b3f8815
Abdoul Karimoun Soumana <idamadou@lab3r2s8.42abudhabi.ae>
2025-11-30 20:46:04 +0400

    Get_Next_Line

---
+++ get_next_line.c
@@ -0,0 +1,126 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: idamadou <idamadou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/19 13:23:26 by idamadou          #+#    #+#             */
+/*   Updated: 2025/11/30 20:43:54 by idamadou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdio.h>
+
+char	*read_and_fill_temp(int fd, char *temp)
+{
+	ssize_t	bytes;
+	char	*buffer;
+	char	*joined;
+
+	buffer = malloc((size_t)BUFFER_SIZE + 1);
+	if (!buffer)
+		return (free(temp), NULL);
+	bytes = 1;
+	while (!ft_strchr(temp, '\n') && bytes > 0)
+	{
+		bytes = read(fd, buffer, BUFFER_SIZE);
+		if (bytes < 0)
+			return (free(buffer), free(temp), NULL);
+		if (bytes == 0)
+			break ;
+		buffer[bytes] = '\0';
+		joined = ft_strjoin(temp, buffer);
+		free(temp);
+		temp = joined;
+		if (!temp)
+			return (free(buffer), NULL);
+	}
+	free(buffer);
+	return (temp);
+}
+
+char	*extract_line(char *temp)
+{
+	size_t	i;
+	size_t	j;
+	char	*line;
+
+	if (!temp || !temp[0])
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (temp[i] && temp[i] != '\n')
+		i++;
+	if (temp[i] == '\n')
+		i++;
+	line = malloc(i + 1);
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (temp[j] && temp[j] != '\n')
+		line[i++] = temp[j++];
+	if (temp[j] == '\n')
+	{
+		line[i] = temp[j];
+		i++;
+	}
+	line[i] = '\0';
+	return (line);
+}
+
+char	*update_temp(char *temp)
+{
+	char	*new_temp;
+	size_t	i;
+	size_t	j;
+
+	if (!temp)
+		return (NULL);
+	i = 0;
+	while (temp[i] && temp[i] != '\n')
+		i++;
+	if (!temp[i])
+		return (free(temp), NULL);
+	i++;
+	if (!temp[i])
+		return (free(temp), NULL);
+	new_temp = malloc(ft_strlen(temp + i) + 1);
+	if (!new_temp)
+		return (free(temp), NULL);
+	j = 0;
+	while (temp[i])
+		new_temp[j++] = temp[i++];
+	new_temp[j] = '\0';
+	free(temp);
+	return (new_temp);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	static char	*temp;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	if (!temp)
+	{
+		temp = malloc(1);
+		if (!temp)
+			return (NULL);
+		temp[0] = '\0';
+	}
+	temp = read_and_fill_temp(fd, temp);
+	if (!temp)
+		return (NULL);
+	line = extract_line(temp);
+	if (!line)
+	{
+		free(temp);
+		temp = NULL;
+		return (NULL);
+	}
+	temp = update_temp(temp);
+	return (line);
+}
---
+++ get_next_line_utils.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: idamadou <idamadou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/19 14:13:40 by idamadou          #+#    #+#             */
+/*   Updated: 2025/11/26 22:30:11 by idamadou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(const char *dest, const char *src)
+{
+	size_t		i;
+	size_t		j;
+	char		*result;
+
+	if (!dest || !src)
+		return (NULL);
+	result = malloc(ft_strlen(dest) + ft_strlen(src) + 1);
+	if (!result)
+		return (NULL);
+	i = 0;
+	while (dest[i])
+	{
+		result[i] = dest[i];
+		i++;
+	}
+	j = 0;
+	while (src[j])
+	{
+		result[i] = src[j];
+		i++;
+		j++;
+	}
+	result[i] = '\0';
+	return (result);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	if ((char)c == '\0')
+		return ((char *)&s[i]);
+	return (NULL);
+}
